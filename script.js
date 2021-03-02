568976630f4f8187dbafd99687d1336c0237afca
assylbek29 <77967674+assylbek29@users.noreply.github.com>
2021-03-02 11:26:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,80 +7,70 @@ const age = document.getElementsByName("age");
 const button = document.getElementById("submit");
 const love_letter = document.getElementById("love_letter");
 
+const getNewPrice = (price, criteria) => {
+    return price * Number(criteria.value);
+}
+
+
+
+const getCheckboxValuesForLoop = (html_collection, price) => { 
+    for (let i=0; i < html_collection.length; i++) {  
+        if (html_collection[i].checked && Number.isInteger(Number(html_collection[i].value))) {
+            price = price + Number(html_collection[i].value)
+        }
+        else if (html_collection[i].checked && !Number.isInteger(html_collection[i].value)) {
+            price = price * Number(html_collection[i].value)
+        }
+    }
+    return price;
+}
+
+
+const getCheckboxValuesFilterReduce = (html_collection, price) => { // create a function that accepts your HTMLCollection of elements and the current price
+    let list = Array.from(html_collection).filter(filteration) // this method turn your HTMLCollection into array
+    let result = list.reduce(reducer, price)
+    return result;
+}
+
+const reducer = (accumulator, item) => {
+    return accumulator + Number(item.value);
+}
+const filteration = (item) => {
+    return item.checked;
+}
+
+
+const getRadioValue = (node_list, price) => {  
+    node_list.forEach(item => {
+        if (item.checked) {
+            price = price * Number(item.value)
+        }
+    })
+    return price;
+}
+
 const calculate = () => {
-    let name = firstName.value; 
+    let name = firstname.value; 
     let price = Number(startingBid.value); 
     let letter = love_letter.value;
     if (name != "") { 
-        price = getNewPrice(price, education);
-        price = getNewPrice(price, networth);
-        price = getNewPrice(price, skills);
-        price = getNewPrice(price, age);
-        price = getNewPrice(price, reputation);
+        price = price * Number(education.value)
+        price = price * Number(networth.value)
+        price = getCheckboxValuesForLoop(skills, price)
+        price = getRadioValue(age, price)
+        price = getCheckboxValuesFilterReduce(reputation, price)
         let person = {
-            fullName: name,
-            finalPrice: price,
-            loveLetter: letter
+            firstname: name,
+            prices: price,
+            love_letter: letter
         }
-        document.getElementById("result").innerHTML = `The price for ${person.fullName} is ${person.finalPrice}. Your love letter is: ${person.loveLetter}`;
+        document.getElementById("result").innerHTML = `The price for ${person.firstname} is ${person.prices}. Your love letter is: ${person.love_letter}`;
     }
     else {
         alert("Name and starting bid cannot be empty");
     }
 }
 
-const getNewPrice = (price, education) => {
-    return price * Number(education.value);
-}
-
-const getNewPrice = (price, networth) => {
-    return price * Number(networth.value);
-}
-
-const getNewPrice = (price, age) => {
-    return price * Number(age.value);
-}
-
-const getNewPrice = (price, reputation) => {
-    return price * Number(reputation.value);
-}
-
-const reducer = (accumulator, skills) => {
-    return accumulator + Number(skills.value);
-}
-const filteration = (skills) => {
-    return skills.checked;
-}
-
-
-const getCheckboxValuesForLoop = (html_collection, price) => { 
-	for (let i=0; i < html_collection.length; i++) {  
-		if (html_collection[i].checked && Number.isInteger(Number(html_collection[i].value))) {
-			price = price + Number(html_collection[i].value)
-		}
-		else if (html_collection[i].checked && !Number.isInteger(html_collection[i].value)) {
-			price = price * Number(html_collection[i].value)
-		}
-	}
-	return price;
-}
-
-const getCheckboxValuesFilterReduce = (html_collection, price) => { 
-    let list = Array.from(html_collection).filter(filteration)
-    let result = list.reduce(reducer, price)
-    return result;
-}
-
-
-
-const getRadioValue = (node_list, price) => {  
-    node_list.forEach(item => {
-        if (age.checked) {
-            price = price * Number(age.value)
-        }
-    })
-    return price;
-}
 
 
 button.addEventListener("click", calculate)
